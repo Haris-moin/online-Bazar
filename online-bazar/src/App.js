ccfbd606bf8d60bc86fce8f7f16c0d7b86466aa5
hmoin <hmoin@NIS-PK223s-MacBook-Pro.local>
2022-01-12 14:54:08 +0500

    developed side nav and layout of dashboard

--- online-bazar/src/App.js
+++ online-bazar/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import LoginContainer from './features/auth/login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignUpContainer from './features/auth/signUp';
+import Layout from './layout';
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" exact component={LoginContainer} />
+          <Route path="/" exact component={Layout} />
           <Route path="/signup" component={SignUpContainer} />
         </Switch>
       </Router>
---
+++ online-bazar/src/dasboard/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TShirt from '../features/clothes/tshirt';
+
+function Dashboard() {
+  return (
+    <div>
+      <TShirt />
+    </div>
+  );
+}
+
+export default Dashboard;
